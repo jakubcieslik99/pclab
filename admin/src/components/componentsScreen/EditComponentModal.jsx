b402c6965b37a2ad990f7c3fbf955f552d423772
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2022-10-19 23:52:35 +0200

    Admin panel ComponentsScreen styling updated

--- admin/src/components/componentsScreen/EditComponentModal.jsx
+++ admin/src/components/componentsScreen/EditComponentModal.jsx
@@ -1,16 +1,96 @@
-import { Fragment } from 'react'
+import { useState, Fragment } from 'react'
 import { Transition, Dialog } from '@headlessui/react'
+import { useDropzone } from 'react-dropzone'
 import { FaTimes } from 'react-icons/fa'
 import Error from '../alerts/Error'
 import Success from '../alerts/Success'
 
+const maxFilesAmount = 1
+
 const EditComponentModal = props => {
+  //variables
+  const [fetchedFiles, setFetchedFiles] = useState([])
+  const [selectedFiles, setSelectedFiles] = useState([])
+
+  const { getRootProps, getInputProps, open } = useDropzone({
+    accept: { 'image/*': ['.png', '.jpg', '.jpeg'] },
+    maxFiles: maxFilesAmount - fetchedFiles.length - selectedFiles.length,
+    maxSize: 5 * 1024 * 1024,
+    noClick: true,
+    noKeyboard: true,
+    onDrop: acceptedFiles => {
+      let preparedFiles = acceptedFiles.map(file =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      )
+      setSelectedFiles(selectedFiles.concat(preparedFiles))
+    },
+  })
+
   //handlers
   const closeHandler = () => {
     props.setIsOpen(false)
     setTimeout(() => {
       props.setEditElement(null)
     }, 250)
+  }
+  const deleteImageHandler = number => {
+    fetchedFiles.splice(number, 1)
+    setFetchedFiles(() => [...fetchedFiles])
+  }
+  const deleteFileHandler = number => {
+    selectedFiles.splice(number, 1)
+    setSelectedFiles(() => [...selectedFiles])
+  }
+  const renderFilesHandler = () => {
+    let thumbs = []
+    for (let i = 0; i < maxFilesAmount; i++) {
+      if (fetchedFiles[i]) {
+        thumbs.push(
+          <div className="mt-[1px] mb-[5px]" key={'image' + i} data-tip="Kliknij aby usunąć zdjęcie">
+            <div className="aspect-[4/3] flex items-center justify-center w-[135px] p-0 border-2 border-gray-400/70 cursor-pointer rounded-xl overflow-hidden">
+              <img
+                src={`${process.env.REACT_APP_API_URL}/static/${fetchedFiles[i]}`}
+                alt="..."
+                onClick={() => deleteImageHandler(i)}
+                className="w-auto h-auto max-w-full max-h-full"
+              />
+            </div>
+          </div>
+        )
+      } else {
+        for (let j = 0; j < maxFilesAmount - i; j++) {
+          if (selectedFiles[j]) {
+            thumbs.push(
+              <div className="mt-[1px] mb-[5px]" key={'file' + j} data-tip="Kliknij aby usunąć zdjęcie">
+                <div className="aspect-[4/3] flex items-center justify-center w-[135px] p-0 border-2 border-gray-400/70 cursor-pointer rounded-xl overflow-hidden">
+                  <img
+                    src={selectedFiles[j].preview}
+                    alt="..."
+                    onClick={() => deleteFileHandler(j)}
+                    className="w-auto h-auto max-w-full max-h-full"
+                  />
+                </div>
+              </div>
+            )
+          } else {
+            thumbs.push(
+              <div className="mt-[1px] mb-[5px]" key={'choose' + j}>
+                <div
+                  className="aspect-[4/3] flex items-center justify-center w-[135px] p-0 text-4xl text-gray-400/80 border-2 border-gray-400/80 border-dashed cursor-pointer rounded-xl overflow-hidden"
+                  onClick={open}
+                >
+                  +
+                </div>
+              </div>
+            )
+          }
+        }
+        return <>{thumbs}</>
+      }
+    }
+    return <>{thumbs}</>
   }
 
   return (
@@ -43,7 +123,13 @@ const EditComponentModal = props => {
                 <form className="flex flex-col w-full col-start-1 row-start-1 px-5 py-4 overflow-hidden bg-gray-200 rounded-lg shadow-md">
                   {/*modal header*/}
                   <div className="flex items-center justify-between w-full gap-4 text-xl font-semibold text-gray-800">
-                    <h2>{props.editElement ? 'Edytuj komponent' : 'Dodaj komponent'}</h2>
+                    <h2 className="flex flex-col">
+                      <span>{props.editElement ? 'Edytuj komponent' : 'Dodaj komponent'}</span>
+                      {props.editElement && (
+                        <span className="text-sm italic font-normal text-gray-700">{'507f1f77bcf86cd799439011'}</span>
+                      )}
+                    </h2>
+
                     <button
                       type="button"
                       onClick={closeHandler}
@@ -59,14 +145,14 @@ const EditComponentModal = props => {
                     <Success isOpen={true} message={'Test success'} />
 
                     <div>
-                      <label htmlFor="profileEmail" className="text-sm">
-                        Podaj aktualny adres email*:
+                      <label htmlFor="componentTitle" className="text-sm">
+                        Podaj tytuł komponentu*:
                       </label>
                       <input
                         type="text"
-                        id="profileEmail"
-                        name="email"
-                        placeholder="Aktualny email*"
+                        id="componentTitle"
+                        name="title"
+                        placeholder="Tytuł komponentu*"
                         className="border-2 border-gray-400/70 rounded-xl bg-white/[0.05] py-2 px-3 w-full transition-colors transition-duration-250 focus:outline-none focus:ring focus:border-gray-800 focus:ring-transparent"
                       />
 
@@ -76,31 +162,14 @@ const EditComponentModal = props => {
                     </div>
 
                     <div>
-                      <label htmlFor="profileNewEmail" className="text-sm">
-                        Podaj nowy adres email:
+                      <label htmlFor="componentURL" className="text-sm">
+                        Podaj URL komponentu*:
                       </label>
                       <input
                         type="text"
-                        id="profileNewEmail"
-                        name="newEmail"
-                        placeholder="Nowy email"
-                        className="border-2 border-gray-400/70 rounded-xl bg-white/[0.05] py-2 px-3 w-full transition-colors transition-duration-250 focus:outline-none focus:ring focus:border-gray-800 focus:ring-transparent"
-                      />
-
-                      <div className="flex flex-col gap-1 mt-[5px]">
-                        <Error isOpen={false} message={'Test error'} />
-                      </div>
-                    </div>
-
-                    <div>
-                      <label htmlFor="profilePassword" className="text-sm">
-                        Podaj aktualne hasło*:
-                      </label>
-                      <input
-                        type="password"
-                        id="profilePassword"
-                        name="password"
-                        placeholder="Aktualne hasło*"
+                        id="componentURL"
+                        name="url"
+                        placeholder="URL komponentu*"
                         className="border-2 border-gray-400/70 rounded-xl bg-white/[0.05] py-2 px-3 w-full transition-colors transition-duration-250 focus:outline-none focus:ring focus:border-gray-800 focus:ring-transparent"
                       />
 
@@ -110,20 +179,49 @@ const EditComponentModal = props => {
                     </div>
 
                     <div>
-                      <label htmlFor="profileNewPassword" className="text-sm">
-                        Podaj nowe hasło:
+                      <label htmlFor="componentPrice" className="text-sm">
+                        Podaj cenę komponentu*:
                       </label>
                       <input
-                        type="password"
-                        id="profileNewPassword"
-                        name="newPassword"
-                        placeholder="Nowe hasło"
+                        type="text"
+                        id="componentPrice"
+                        name="price"
+                        placeholder="0000,00 zł*"
                         className="border-2 border-gray-400/70 rounded-xl bg-white/[0.05] py-2 px-3 w-full transition-colors transition-duration-250 focus:outline-none focus:ring focus:border-gray-800 focus:ring-transparent"
                       />
 
                       <div className="flex flex-col gap-1 mt-[5px]">
                         <Error isOpen={false} message={'Test error'} />
                       </div>
+                    </div>
+
+                    <div>
+                      <label htmlFor="componentAmount" className="text-sm">
+                        Podaj dostępną ilość sztuk komponentu*:
+                      </label>
+                      <input
+                        type="text"
+                        id="componentAmount"
+                        name="amount"
+                        placeholder="0 szt.*"
+                        className="border-2 border-gray-400/70 rounded-xl bg-white/[0.05] py-2 px-3 w-full transition-colors transition-duration-250 focus:outline-none focus:ring focus:border-gray-800 focus:ring-transparent"
+                      />
+
+                      <div className="flex flex-col gap-1 mt-[5px]">
+                        <Error isOpen={false} message={'Test error'} />
+                      </div>
+                    </div>
+
+                    <div>
+                      <label htmlFor="componentsImage" className="text-sm">
+                        Wgraj zdjęcie poglądowe:
+                      </label>
+                      <div {...getRootProps()} className="flex flex-row flex-wrap">
+                        <input id="componentsImage" name="image" {...getInputProps()} />
+                        {renderFilesHandler()}
+                      </div>
+
+                      <p className="text-[11px] text-gray-600">Maksymalna wielkość zdjęcia (.png/.jpg/.jpeg) to 5 MB.</p>
                     </div>
                   </div>
 
