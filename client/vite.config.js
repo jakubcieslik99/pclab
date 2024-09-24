5c35b4b99451d27af46dd8ede1f775dfec19dab8
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2024-09-25 01:32:58 +0200

    Updated client dependencies, eslint config & Docker files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +10,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    strictPort: true,
+  },
+  preview: {
+    // eslint-disable-next-line no-undef
+    port: process.env.VITE_PREVIEW_PORT ? parseInt(process.env.VITE_PREVIEW_PORT) : 3000,
     strictPort: true,
   },
 })
