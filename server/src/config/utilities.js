b46737c79b7972716fc6633a0f36c17c794b30a9
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2025-03-24 00:27:08 +0100

    Updated server dependencies, dependency manager, mailing & secrets handling

--- server/src/config/utilities.js
+++ server/src/config/utilities.js
@@ -1,14 +1,65 @@
 import dotenv from 'dotenv'
+import { InfisicalSDK } from '@infisical/sdk'
 import logger from 'pino'
 
 dotenv.config()
+
+const loadSecrets = async () => {
+  const infisicalCredentials = {
+    siteUrl: process.env.INFISICAL_URL,
+    clientId: process.env.INFISICAL_CLIENT_ID,
+    clientSecret: process.env.INFISICAL_CLIENT_SECRET,
+    projectId: process.env.INFISICAL_PROJECT_ID,
+  }
+
+  if (process.env.ENV !== 'production') {
+    return {
+      MONGO_PASSWORD: process.env.MONGO_PASSWORD || 'Passw0rd!',
+      JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET || 'JWT_ACCESS_TOKEN_SECRET',
+      JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET || 'JWT_REFRESH_TOKEN_SECRET',
+      CRYPTO_SECRET: process.env.CRYPTO_SECRET || 'CRYPTO_SECRET',
+      STRIPE_SECRET: process.env.STRIPE_SECRET,
+      STRIPE_ENDPOINT_SECRET: process.env.STRIPE_ENDPOINT_SECRET,
+      SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+    }
+  }
+
+  const client = new InfisicalSDK({ siteUrl: infisicalCredentials.siteUrl })
+
+  await client
+    .auth()
+    .universalAuth.login({
+      clientId: infisicalCredentials.clientId,
+      clientSecret: infisicalCredentials.clientSecret,
+    })
+    .catch(error => {
+      throw new Error(`Failed to log in to Infisical: ${error}`)
+    })
+
+  const { secrets } = await client
+    .secrets()
+    .listSecrets({
+      environment: process.env.ENV,
+      projectId: infisicalCredentials.projectId,
+      recursive: false,
+      secretPath: '/server',
+    })
+    .catch(error => {
+      throw new Error(`Failed to fetch secrets from Infisical: ${error}`)
+    })
+
+  return secrets.reduce((acc, item) => {
+    acc[item.secretKey] = item.secretValue
+    return acc
+  }, {})
+}
 
 const config = {
   ENV: process.env.ENV || 'development',
   // Node.js
   HOST: process.env.HOST || 'localhost',
-  PORT: process.env.PORT || 3002,
-  API_URL: process.env.API_URL || 'http:// localhost:3002',
+  PORT: process.env.PORT || 4000,
+  API_URL: process.env.API_URL || 'http:// localhost:4000',
   APP_URL: process.env.APP_URL || 'http:// localhost:3000',
   ADMINPANEL_URL: process.env.ADMINPANEL_URL || 'http:// localhost:3001',
   // MongoDB
@@ -16,16 +67,12 @@ const config = {
   MONGO_PORT: process.env.MONGO_PORT || 27017,
   MONGO_DB: process.env.MONGO_DB || 'database',
   MONGO_USER: process.env.MONGO_USER || 'root',
-  MONGO_PASSWORD: process.env.MONGO_PASSWORD || 'Passw0rd!',
   // Other
-  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET || 'JWT_ACCESS_TOKEN_SECRET',
-  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET || 'JWT_REFRESH_TOKEN_SECRET',
-  CRYPTO_SECRET: process.env.CRYPTO_SECRET || 'CRYPTO_SECRET',
-  STRIPE_SECRET: process.env.STRIPE_SECRET,
-  STRIPE_ENDPOINT_SECRET: process.env.STRIPE_ENDPOINT_SECRET,
-  GMAIL_ADDRESS: process.env.GMAIL_ADDRESS,
-  GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
-  NOREPLY_ADDRESS: process.env.NOREPLY_ADDRESS || 'noreply@pclab.com',
+  SMTP_HOST: process.env.SMTP_HOST,
+  SMTP_USERNAME: process.env.SMTP_USERNAME,
+  NOREPLY_ADDRESS: process.env.NOREPLY_ADDRESS,
+  // Secrets
+  ...(await loadSecrets()),
 }
 
 const log = logger({
